277fddc644d6e72016635bf7bafdf1d225ffc383
John Jacobsen <eigenhombre@gmail.com>
2023-02-28 20:43:17 -0600

    Start using actual test cases as examples.

--- main_test.go
+++ main_test.go
@@ -1,30 +1,60 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"testing"
 )
 
 func TestStuff(t *testing.T) {
-	IN := func(s ...string) []string { return s }
-	examples := []struct {
-		input  []string
-		output string
-	}{
-		{IN("1", "2", "+"), ""},
-		// State is saved!
-		{IN("."), "3"},
-		// YAH
-		//		{IN("1", "2", "+", "."), "3"},
+	type example struct {
+		input     []string
+		output    string
+		canonical bool
+	}
+	Case := func(params ...string) example {
+		output := params[len(params)-1]
+		input := params[:len(params)-1]
+		return example{input, output, false}
+	}
+	ECase := func(params ...string) example {
+		output := params[len(params)-1]
+		input := params[:len(params)-1]
+		return example{input, output, true}
+	}
+
+	examples := []example{
+		Case("1", "drop", ""),
+		ECase("1", "2", "+", ""),
+		ECase(".", "3"),
+		Case("1", ".", "1"),
+		Case("1", "2", "+", ".", "3"),
 	}
 	i := newInterpreter()
+	// Save ECases to a file examples.fs
+	// open file
+	outfile := "examples.fs"
+	f, err := os.Create(outfile)
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+	defer f.Close()
 	for _, e := range examples {
+		output := ""
 		for _, input := range e.input {
-			output, err := i.handleInputLine(input)
+			out, err := i.handleInputLine(input)
 			if err != nil {
 				t.Errorf("error: %v", err)
 			}
-			if output != e.output {
-				t.Errorf("expected '%v', got '%v'", e.output, output)
+			output += out
+		}
+		if output != e.output {
+			t.Errorf("expected '%v', got '%v'", e.output, output)
+		}
+		if e.canonical {
+			f.WriteString("> " + strings.Join(e.input, " ") + "\n")
+			if e.output != "" {
+				f.WriteString(e.output + "\n")
 			}
 		}
 	}
--- updatereadme.py
+++ updatereadme.py
@@ -1,28 +1,12 @@
 # Adapted from https://stackoverflow.com/questions/33365621:
 import re
 
-def execute(code):
-    import subprocess
-    proc = subprocess.Popen(code,
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
-    out, _ = proc.communicate()  # Optimism!!!
-    return out.decode('utf-8')
-
-steps = ["go build .",
-         "go install .",
-         "echo '2 2 * .' | toobeci"]
-
-steps_txt = ("\n" +
-             "".join([f"$ {s}\n{execute(s)}\n" for s in steps]) +
-             "\n")
+# Read in examples.fs:
+with open('examples.fs', 'r') as fs:
+    examples = fs.read()
 
 with open('README.md', 'r') as md:
         readme = md.read()
-        examples = ("\n    " +
-                    "\n    ".join([l.rstrip() for l in steps_txt.split('\n')]) +
-                    "\n")
         ntext = re.sub(r'(?<=BEGIN EXAMPLES \-\-\>\n)(.*)(?=\<\!\-\- END EXAMPLES)',
                        examples,
                        readme,
