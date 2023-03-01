eced8a7f5defc73d3029eaf1c22225c86ece5e20
John Jacobsen <eigenhombre@gmail.com>
2023-02-28 19:13:12 -0600

    First math functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -31,7 +32,47 @@ func (s stringElement) String() string {
 	return string(s)
 }
 
+func applyBinOp(s *stack, op func(a, b stackElement) stackElement) (*stack, error) {
+	e1, err := s.pop()
+	if err != nil {
+		return s, err
+	}
+	e2, err := s.pop()
+	if err != nil {
+		return s, err
+	}
+	switch e1.(type) {
+	case intElement:
+		switch e2.(type) {
+		case intElement:
+			s.push(op(e1, e2))
+			return s, nil
+		}
+	}
+	return s, fmt.Errorf("type error")
+}
+
 var builtins = map[string]func(*stack) (*stack, error){
+	"+": func(s *stack) (*stack, error) {
+		return applyBinOp(s, func(a, b stackElement) stackElement {
+			return intElement(a.(intElement) + b.(intElement))
+		})
+	},
+	"-": func(s *stack) (*stack, error) {
+		return applyBinOp(s, func(a, b stackElement) stackElement {
+			return intElement(a.(intElement) - b.(intElement))
+		})
+	},
+	"*": func(s *stack) (*stack, error) {
+		return applyBinOp(s, func(a, b stackElement) stackElement {
+			return intElement(a.(intElement) * b.(intElement))
+		})
+	},
+	"/": func(s *stack) (*stack, error) {
+		return applyBinOp(s, func(a, b stackElement) stackElement {
+			return intElement(a.(intElement) / b.(intElement))
+		})
+	},
 	"drop": func(s *stack) (*stack, error) {
 		_, err := s.pop()
 		return s, err
@@ -53,12 +94,29 @@ var builtins = map[string]func(*stack) (*stack, error){
 		fmt.Println(e)
 		return s, nil
 	},
+	"emit": func(s *stack) (*stack, error) {
+		e, err := s.pop()
+		if err != nil {
+			return s, err
+		}
+		switch e := e.(type) {
+		case intElement:
+			fmt.Printf("%c", e)
+			return s, nil
+		default:
+			return s, fmt.Errorf("type error")
+		}
+	},
+	".s": func(s *stack) (*stack, error) {
+		fmt.Print(s)
+		return s, nil
+	},
 }
 
 func (s *stack) String() string {
 	ret := ""
 	for i, e := range s.elements {
-		ret += fmt.Sprintf("\t%d %v\n", i, e)
+		ret += fmt.Sprintf("\t%d %v (%T)\n", i, e, e)
 	}
 	return ret
 }
@@ -79,37 +137,49 @@ func (s *stack) pop() (stackElement, error) {
 
 func main() {
 	// print a preamble
-	fmt.Print("Welcome to Toobeci.\n\n")
+	fmt.Print("Welcome to Toobeci.\n")
+
 	rdr := bufio.NewReader(os.Stdin)
 	// create a stack
 	s := &stack{}
-top:
-	// print a prompt
-	fmt.Print("> ")
-	// read a line of input from stdin
-	input, err := rdr.ReadString('\n')
-	if err != nil {
-		if err.Error() == "EOF" {
-			fmt.Println("\nGoodbye.")
-			return
-		}
-		fmt.Println(err)
-		goto top
-	}
-	trimmed := strings.Trim(input, " \t\n")
-	if trimmed != "" {
-		// See if input is in dictionary
-		if f, ok := builtins[trimmed]; ok {
-			// If so, call it
-			s, err = f(s)
-			if err != nil {
-				fmt.Println(err)
+outer:
+	for {
+		// print a prompt
+		fmt.Print("\n> ")
+		// read a line of input from stdin
+		input, err := rdr.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("\nGoodbye.")
+				return
 			}
-			fmt.Print(s)
-			goto top
+			fmt.Println(err)
+			continue
 		}
-		s.push(stringElement(strings.Trim(trimmed, " \t\n")))
+		trimmed := strings.Trim(input, " \t\n")
+		// Split on whitespace
+		words := strings.Split(trimmed, " ")
+		// fmt.Println("words:", words)
+		for _, word := range words {
+			// fmt.Println("handling word", word)
+			// See if input is in dictionary
+			if f, ok := builtins[word]; ok {
+				// If so, call it
+				s, err = f(s)
+				if err != nil {
+					fmt.Println(err)
+					continue outer
+				}
+				continue
+			}
+			// Not in the dictionary?  Try to parse it
+			// as an int
+			if i, err := strconv.Atoi(word); err == nil {
+				s.push(intElement(i))
+				continue
+			}
+			s.push(stringElement(word))
+		}
 		fmt.Print(s)
 	}
-	goto top
 }
